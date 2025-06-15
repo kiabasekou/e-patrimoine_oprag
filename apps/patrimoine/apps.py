c84fd93d67d9d77d55b56a2db767d2b28aa75d62
kiabasekou <souare.ahmed@gmail.com>
2025-06-15 04:11:00 +0200

    🔄 Mise à jour automatique - 2025-06-15 04:10:59
    Sauvegarde automatique effectuée par le script backup_sync.sh
    - Sauvegarde locale créée: e-patrimoine_oprag_backup_20250615_041055
    - Timestamp: 20250615_041055

--- apps/patrimoine/apps.py
+++ apps/patrimoine/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PatrimoineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'patrimoine'
+    name = 'apps.patrimoine'
